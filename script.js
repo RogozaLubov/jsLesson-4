6c48cee4ecb848bd56535bc5c8bae7b3053e669c
Rogoza Lyuba <lyubashka22rogoza@gmail.com>
2021-08-17 00:54:12 +0300

    add task 2 ,3

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ let reduceValue = arrOne.reduce(function( previousValue, item, index, array ){
     return item + previousValue ;
 }, 0);
 console.log (reduceValue);
+
+//task2//
+
+let arrTwo = ['Ann', 'Baddy', 'Sem',];
+console.log (arrTwo.indexOf('Baddy'));
+console.log (arrTwo.indexOf('Lyubov'));
+console.log (arrTwo.indexOf('Ann',2));
+
+//task3//
+let arrThree = [1,2,3];
+let arrFour = [4,5,6];
+let arrFive = arrThree.concat(arrFour);
+console.log (arrFive);
